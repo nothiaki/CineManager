1b562c8dc0d780db132b6bc28a678a820e68c8b1
nothiaki <celsohenrique367@gmail.com>
2025-06-21 17:59:02 -0300

    feat(cine-manager-api): init project

---
+++ cine-manager-api/src/main/java/com/example/cine_manager_api/CineManagerApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.cine_manager_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CineManagerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CineManagerApiApplication.class, args);
+	}
+
+}
---
+++ cine-manager-api/src/test/java/com/example/cine_manager_api/CineManagerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cine_manager_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CineManagerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
