3fbcee18b133a71d820223b5d25600c11a1b4e81
nothiaki <celsohenrique367@gmail.com>
2025-06-22 17:47:00 -0300

    feat(cine-manager-api): user domain

---
+++ cine-manager-api/src/main/java/com/example/cine_manager_api/modules/user/domain/User.java
@@ -0,0 +1,105 @@
+package com.example.cine_manager_api.modules.user.domain;
+
+import java.time.LocalDate;
+import java.util.UUID;
+import com.example.cine_manager_api.modules.user.enums.Role;
+
+public class User {
+
+  private UUID id;
+
+  private String email;
+  private String fullName;
+  private String username;
+  private String passwordHash;
+
+  private LocalDate birth;
+  private LocalDate createdAt;
+  private LocalDate updatedAt;
+
+  private Role role;
+  private boolean isActive;
+
+  public User() {}
+
+  public UUID getId() {
+    return id;
+  }
+
+  public void setId(UUID id) {
+    this.id = id;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public String getFullName() {
+    return fullName;
+  }
+
+  public void setFullName(String fullName) {
+    this.fullName = fullName;
+  }
+
+  public String getUsername() {
+    return username;
+  }
+
+  public void setUsername(String username) {
+    this.username = username;
+  }
+
+  public String getPasswordHash() {
+    return passwordHash;
+  }
+
+  public void setPasswordHash(String passwordHash) {
+    this.passwordHash = passwordHash;
+  }
+
+  public LocalDate getBirth() {
+    return birth;
+  }
+
+  public void setBirth(LocalDate birth) {
+    this.birth = birth;
+  }
+
+  public LocalDate getCreatedAt() {
+    return createdAt;
+  }
+
+  public void setCreatedAt(LocalDate createdAt) {
+    this.createdAt = createdAt;
+  }
+
+  public LocalDate getUpdatedAt() {
+    return updatedAt;
+  }
+
+  public void setUpdatedAt(LocalDate updatedAt) {
+    this.updatedAt = updatedAt;
+  }
+
+  public Role getRole() {
+    return role;
+  }
+
+  public void setRole(Role role) {
+    this.role = role;
+  }
+
+  public boolean isActive() {
+    return isActive;
+  }
+
+  public void setActive(boolean isActive) {
+    this.isActive = isActive;
+  }
+}
+
---
+++ cine-manager-api/src/main/java/com/example/cine_manager_api/modules/user/enums/Role.java
@@ -0,0 +1,8 @@
+package com.example.cine_manager_api.modules.user.enums;
+
+public enum Role {
+
+  COSTUMER,
+  ADMIN
+
+}
