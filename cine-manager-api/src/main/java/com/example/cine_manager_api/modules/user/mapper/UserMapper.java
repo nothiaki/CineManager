65da1bf18d136377ac1c48a7835e1fe90254b358
nothiaki <celsohenrique367@gmail.com>
2025-06-24 22:41:47 -0300

    feat(cine-manager-service): jpa mapper

--- cine-manager-api/src/main/java/com/example/cine_manager_api/modules/user/mapper/UserMapper.java
+++ cine-manager-api/src/main/java/com/example/cine_manager_api/modules/user/mapper/UserMapper.java
@@ -3,29 +3,22 @@ package com.example.cine_manager_api.modules.user.mapper;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
-import com.example.cine_manager_api.modules.user.domain.User;
+import com.example.cine_manager_api.modules.user.adapter.outbound.persistence.JpaUserEntity;
+import com.example.cine_manager_api.modules.user.domain.user.User;
 import com.example.cine_manager_api.modules.user.dto.RequestCreateUserDto;
 import com.example.cine_manager_api.modules.user.dto.ResponseCreateUserDto;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
 
-  @Mapping(source = "email", target = "email")
-  @Mapping(source = "fullName", target = "fullName")
-  @Mapping(source = "username", target = "username")
   @Mapping(source = "password", target = "passwordHash")
-  @Mapping(source = "birth", target = "birth")
   public User requestCreateUserDtoToDomain(RequestCreateUserDto requestCreateUserDto);
 
-  @Mapping(source = "id", target = "id")
-  @Mapping(source = "email", target = "email")
-  @Mapping(source = "fullName", target = "fullName")
-  @Mapping(source = "username", target = "username")
-  @Mapping(source = "birth", target = "birth")
-  @Mapping(source = "createdAt", target = "createdAt")
-  @Mapping(source = "updatedAt", target = "updatedAt")
-  @Mapping(source = "role", target = "role")
   public ResponseCreateUserDto domainToResponseCreateUserDto(User user);
+
+  public User jpaUserEntityToDomain(JpaUserEntity jpaUserEntity);
+
+  public JpaUserEntity domainToJpaUserEntity(User user);
 
 }
 
